de31b7b970abd9125ab3f16888ba460521a3ad22
Guillaume C <guillaume.chebib@hotmail.fr>
2021-03-03 14:45:38 +0100

    Initial Commit

---
+++ Shared/TestAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppIOSApp.swift
+//  Shared
+//
+//  Created by user190184 on 03/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
